495eb71879a561510848e2d130e6bb72e81c542d
Francesco Bartolomeo <116885550+Frankesko@users.noreply.github.com>
2024-09-13 10:47:02 +0000

    dasdasddd

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import {
 import { db } from "./firebase";
 import { ref, onValue, push, update, remove } from "firebase/database";
 import "./App.css";
-import Login from "./login";
+import Login from "./login.jsx";
 
 const AddTopicPopup = ({ onClose, onAddTopic, topic = null }) => {
   const [topicTitle, setTopicTitle] = useState(topic ? topic.title : "");
